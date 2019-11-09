c3eeea273c3a2a85abda22f1a260f45d6f7c5d42
Alek Slater <slayte@gmail.com>
2019-11-09 14:29:52 +0100

    disables dartfm in vscode proj. added editor config for tabs over spaces for dart

---
+++ lib/objects.dart
@@ -0,0 +1,29 @@
+class SimpleClass
+{
+	var testing = "";
+	String testing2 = "";  
+}
+
+void ifs()
+{
+	var isTrue = true;
+	if (isTrue)
+		print("its true");
+
+  if (!isTrue)
+	{
+		print("its false");
+  }
+	else
+	{
+		print("its true");
+  }
+}
+
+void fors()
+{
+	for(var i=0; i<2; i++)
+	{
+		print("i is $i");
+	}
+}
